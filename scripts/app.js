c51a522d7ac68e03d0027e510b43165025d15531
LeThanhVu1995 <lethanhvu20015@gmail.com>
2022-05-17 06:39:56 +0700

    update fix scroll

--- scripts/app.js
+++ scripts/app.js
@@ -21,7 +21,7 @@ $(".team-gallery").slick({
 $(".team-description-list").slick({
   dots: true,
   slidesToShow: 1,
-  infinite: true,
+  infinite: false,
   arrows: true,
   initialSlide: 3,
   focusOnSelect: true,
@@ -30,7 +30,6 @@ $(".team-description-list").slick({
   nextArrow: `<button type='button' class='slick-next pull-right'><span class="icon-chevron-right"></button>`,
 });
 $(document).ready(function () {
-
   $(".mb-navigation-list-menu-item-text").on("click", () => {
     $("body").removeClass("nav-active");
   });
@@ -48,48 +47,48 @@ function scrollHorizontal(classname) {
 }
 console.clear();
 const app = (() => {
-    let body;
-    let menu;
-    let menuItems;
-    let nav;
-    const init = () => {
-        body = document.querySelector("body");
-        menu = document.querySelector(".menu-icon");
-        nav = document.querySelector(".nav");
-        applyListeners();
-    };
-    const applyListeners = () => {
-        menu.addEventListener("click", () => toggleClass(body, "nav-active"));
-    };
-    const toggleClass = (element, stringClass) => {
-        if (element.classList.contains(stringClass))
-            element.classList.remove(stringClass);
-        else element.classList.add(stringClass);
-    };
-    init();
+  let body;
+  let menu;
+  let menuItems;
+  let nav;
+  const init = () => {
+    body = document.querySelector("body");
+    menu = document.querySelector(".menu-icon");
+    nav = document.querySelector(".nav");
+    applyListeners();
+  };
+  const applyListeners = () => {
+    menu.addEventListener("click", () => toggleClass(body, "nav-active"));
+  };
+  const toggleClass = (element, stringClass) => {
+    if (element.classList.contains(stringClass))
+      element.classList.remove(stringClass);
+    else element.classList.add(stringClass);
+  };
+  init();
 })();
 const sections = document.querySelectorAll("section[id]");
 
-  function scrollActive() {
-    const scrollY = window.pageYOffset;
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
-      const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 120,
-        sectionId = current.getAttribute("id");
-      console.log(current.offsetTop)
-      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-        document
-          .querySelector(".mb-navigation-list-menu a[href*=" + sectionId + "]")
-          .classList.add("active");
-      } else {
-        document
-          .querySelector(".mb-navigation-list-menu a[href*=" + sectionId + "]")
-          .classList.remove("active");
-      }
-    });
-  }
-  window.addEventListener("scroll", scrollActive);
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 120,
+      sectionId = current.getAttribute("id");
+    console.log(current.offsetTop);
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".mb-navigation-list-menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".mb-navigation-list-menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 function countdown() {
   var countDownDate = new Date("Aug 5, 2022 15:37:25").getTime();
 
@@ -149,17 +148,16 @@ const swiper = new Swiper(".feature-list", {
   },
 });
 
-
 var canvas = document.createElement("canvas");
-var width = canvas.width = window.innerWidth * 0.75;
-var height = canvas.height = window.innerHeight * 0.75;
-document.getElementsByClassName('first-view')[0].appendChild(canvas);
-var gl = canvas.getContext('webgl');
+var width = (canvas.width = window.innerWidth * 0.75);
+var height = (canvas.height = window.innerHeight * 0.75);
+document.getElementsByClassName("first-view")[0].appendChild(canvas);
+var gl = canvas.getContext("webgl");
 
-var mouse = {x: 0, y: 0};
+var mouse = { x: 0, y: 0 };
 
-var numMetaballs
-if(window.innerWidth > 992) {
+var numMetaballs;
+if (window.innerWidth > 992) {
   numMetaballs = 25;
 } else {
   numMetaballs = 8;
@@ -173,7 +171,7 @@ for (var i = 0; i < numMetaballs; i++) {
     y: Math.random() * (height - 2 * radius) + radius,
     vx: (Math.random() - 0.5) * 3,
     vy: (Math.random() - 0.5) * 3,
-    r: radius * 0.75
+    r: radius * 0.75,
   });
 }
 
@@ -187,20 +185,29 @@ gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
-var fragmentShaderSrc = `
+var fragmentShaderSrc =
+  `
 precision highp float;
 
-const float WIDTH = ` + (width >> 0) + `.0;
-const float HEIGHT = ` + (height >> 0) + `.0;
+const float WIDTH = ` +
+  (width >> 0) +
+  `.0;
+const float HEIGHT = ` +
+  (height >> 0) +
+  `.0;
 
-uniform vec3 metaballs[` + numMetaballs + `];
+uniform vec3 metaballs[` +
+  numMetaballs +
+  `];
 
 void main(){
 float x = gl_FragCoord.x;
 float y = gl_FragCoord.y;
 
 float sum = 0.0;
-for (int i = 0; i < ` + numMetaballs + `; i++) {
+for (int i = 0; i < ` +
+  numMetaballs +
+  `; i++) {
 vec3 metaball = metaballs[i];
 float dx = metaball.x - x;
 float dy = metaball.y - y;
@@ -229,26 +236,31 @@ gl.linkProgram(program);
 gl.useProgram(program);
 
 var vertexData = new Float32Array([
-  -1.0,  1.0, // top left
-  -1.0, -1.0, // bottom left
-  1.0,  1.0, // top right
-  1.0, -1.0, // bottom right
+  -1.0,
+  1.0, // top left
+  -1.0,
+  -1.0, // bottom left
+  1.0,
+  1.0, // top right
+  1.0,
+  -1.0, // bottom right
 ]);
 var vertexDataBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
 
-var positionHandle = getAttribLocation(program, 'position');
+var positionHandle = getAttribLocation(program, "position");
 gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
-                       2, // position is a vec2
-                       gl.FLOAT, // each component is a float
-                       gl.FALSE, // don't normalize values
-                       2 * 4, // two 4 byte float components per vertex
-                       0 // offset into each span of vertex data
-                      );
+gl.vertexAttribPointer(
+  positionHandle,
+  2, // position is a vec2
+  gl.FLOAT, // each component is a float
+  gl.FALSE, // don't normalize values
+  2 * 4, // two 4 byte float components per vertex
+  0 // offset into each span of vertex data
+);
 
-var metaballsHandle = getUniformLocation(program, 'metaballs');
+var metaballsHandle = getUniformLocation(program, "metaballs");
 
 loop();
 function loop() {
@@ -257,8 +269,10 @@ function loop() {
     metaball.x += metaball.vx;
     metaball.y += metaball.vy;
 
-    if (metaball.x < metaball.r || metaball.x > width - metaball.r) metaball.vx *= -1;
-    if (metaball.y < metaball.r || metaball.y > height - metaball.r) metaball.vy *= -1;
+    if (metaball.x < metaball.r || metaball.x > width - metaball.r)
+      metaball.vx *= -1;
+    if (metaball.y < metaball.r || metaball.y > height - metaball.r)
+      metaball.vy *= -1;
   }
 
   var dataToSendToGPU = new Float32Array(3 * numMetaballs);
@@ -270,7 +284,7 @@ function loop() {
     dataToSendToGPU[baseIndex + 2] = mb.r;
   }
   gl.uniform3fv(metaballsHandle, dataToSendToGPU);
-  
+
   //Draw
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
@@ -292,7 +306,7 @@ function compileShader(shaderSource, shaderType) {
 function getUniformLocation(program, name) {
   var uniformLocation = gl.getUniformLocation(program, name);
   if (uniformLocation === -1) {
-    throw 'Can not find uniform ' + name + '.';
+    throw "Can not find uniform " + name + ".";
   }
   return uniformLocation;
 }
@@ -300,14 +314,12 @@ function getUniformLocation(program, name) {
 function getAttribLocation(program, name) {
   var attributeLocation = gl.getAttribLocation(program, name);
   if (attributeLocation === -1) {
-    throw 'Can not find attribute ' + name + '.';
+    throw "Can not find attribute " + name + ".";
   }
   return attributeLocation;
 }
 
-canvas.onmousemove = function(e) {
+canvas.onmousemove = function (e) {
   mouse.x = e.clientX;
   mouse.y = e.clientY;
-}
-
-
+};
